e44d4ace4ee8b3887b196686fcbbbfcb33a81b07
David Hermanski <david.hermanski@gmail.com>
2015-03-14 14:13:32 -0500

    finished code, added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R includes two fuctions: "makeCacheMatrix" and "cacheSolve"
+##
+## "makeCacheMatrix" accepts a matix (assumed to be "invertible") as input
+## and creates a special "matrix" object that can cache its inverse.   
+##
+## "cacheSolve" computes the inverse of the special "matrix" returned by "makeCacheMatrix".
+## If the inverse has already been calculated (and the matrix has not changed) then "cacheSolve"
+## retrieves the inverse from cache.
 
-## Write a short comment describing this function
+## "makeCacheMatrix" - accpets a matrix (assumed to be "invertible") as input and creates
+## a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(input_matrix = matrix()) {
+	
+	stored_inverse <- NULL
 
+	set <- function(y) {
+		input_matrix <<- y
+		stored_inverse <<- NULL
+	}
+
+	get <- function() {
+		return (input_matrix)
+	}
+
+	setinverse <- function(sent_replacement_inverse) {
+			stored_inverse <<- sent_replacement_inverse
+	}
+
+	getinverse <- function() {
+		return (stored_inverse)
+	}
+
+	list (set = set, get = get,
+		setinverse = setinverse,
+		getinverse = getinverse)
 }
 
+## "cacheSolve" computes and returns the inverse of the special "matrix" returned by 
+## "makeCacheMatrix".  If the inverse has already been cacluated then "cacheSolve" simply 
+## returns the inverse from cache.
 
-## Write a short comment describing this function
+cacheSolve <- function(made_matrix,...) {
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	local_inverse  <- made_matrix$getinverse()
+	
+	if (!is.null(local_inverse)) {
+		message("getting cached data")
+		return(local_inverse)
+	}
+	else {
+		local_matrix <- made_matrix$get()
+		local_inverse <- solve(local_matrix, ...)
+		made_matrix$setinverse(local_inverse)
+		return(local_inverse)
+		}
 }
